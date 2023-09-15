a73c8449e3d55671041047dd3d8a88cf188702ca
Kyle Butts <buttskyle96@gmail.com>
2023-09-15 09:15:51 -0400

    Initial switch to Starlight

---
+++ astro.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig } from "astro/config"
+import starlight from "@astrojs/starlight"
+
+import tailwind from "@astrojs/tailwind"
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: "stata2R",
+			social: {
+				github: "https://github.com/stata2r/stata2r.github.io/",
+			},
+			customCss: ["./src/styles/main.css"],
+			sidebar: [
+				{
+					label: "Guides",
+					items: [
+            {
+							label: "Intro",
+							link: "/",
+						},
+						{
+							label: "data.table",
+							link: "/guides/data_table/",
+						},
+						{
+							label: "fixest",
+							link: "/guides/fixest/",
+						},
+						{
+							label: "extras",
+							link: "/guides/extras/",
+						},
+					],
+				},
+			],
+		}),
+		tailwind({
+      applyBaseStyles: false,
+    }),
+	],
+})
